e9f699b345919706cb88ad01d717bfc9c0c7afb3
malethik <quintiliani.d@gmail.com>
2024-04-30 14:45:36 +0200

    added upload method

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,11 @@ import { UserController } from './user.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { CoreModule } from '../core/core.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { FileService } from '../core/file/file.service';
 
 @Module({
   imports: [PrismaModule, CoreModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, FileService],
 })
 export class UserModule {}
